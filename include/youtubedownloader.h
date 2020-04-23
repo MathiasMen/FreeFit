3d3e9675ddf051def526bff3e894b4f0465858a4
MathiasMen <mathiasmentler@web.de>
2020-04-23 09:10:00 +0200

    Created functionality to cut video to specified time tags. Added tests that assert that the output videos exist.

--- include/youtubedownloader.h
+++ include/youtubedownloader.h
@@ -1,3 +1,5 @@
+#pragma once 
+
 #include <string>
 #include <iostream>
 #include "include/youtubeoptions.h"
@@ -8,6 +10,7 @@ public:
     YoutubeDownloader(std::string t_path_to_executable) : path_to_executable(t_path_to_executable){}
     virtual void download(std::string yt_url, std::string out_path) = 0;
     virtual void download(std::string yt_url, std::string out_path, YoutubeOptions opt) = 0;
+    virtual void download(std::string yt_url, std::string out_path, int start_time, int end_time, YoutubeOptions opt = YoutubeOptions()) = 0;
 protected:
     std::string path_to_executable;
 private:
@@ -26,14 +29,28 @@ public:
 
     void download(std::string yt_url, std::string out_path, YoutubeOptions opt) override
     {
-        std::string command(this->path_to_executable +
+        std::string dl_cmnd(this->path_to_executable +
                             opt.getOptions() +
-                            " -o \"" +
+                            " -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]' -o \"" +
                             out_path +
-                            "/%(title)s.%(ext)s\" \"" +
+                            "\" \"" +
                             yt_url +
                             "\""); 
-        std::system(command.c_str());
+        std::system(dl_cmnd.c_str());
+    }
+
+    void download(std::string yt_url, std::string out_path, int start_time, int end_time, YoutubeOptions opt = YoutubeOptions()) override
+    {
+        download(yt_url,out_path, opt);
+        std::string cut_cmnd("ffmpeg -ss " + 
+                            std::to_string(start_time) + 
+                            " -i " +
+                            out_path + 
+                            " -to " +
+                            std::to_string(end_time) + 
+                            " -c copy " +
+                            out_path + "_resized.mp4");
+        std::system(cut_cmnd.c_str());
     }
 private:
     YoutubeDL();
--- test/youtubeDownloadTest.in.cpp
+++ test/youtubeDownloadTest.in.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <fstream>
 #include "include/youtubeoptions.h"
 #include "include/youtubedownloader.h"
 
@@ -18,8 +19,20 @@ TEST(YoutubeDownload, Options)
     EXPECT_EQ(opt.getOptions()," --help --version --someOtherArgument");
 }
 
-TEST(YoutubeDownload, Downloader)
+TEST(YoutubeDownload, Download)
 {
+    std::string out_path = {"${CMAKE_BINARY_DIR}/test/FlorenceAndTheMachine.mp4"};
     YoutubeDownloader* d = new YoutubeDL("${CMAKE_SOURCE_DIR}/tools/YoutubeDL/youtube-dl");
-    d->download("https://www.youtube.com/watch?v=nyiNSFp2uf0","${CMAKE_BINARY_DIR}/test");
+    d->download("https://www.youtube.com/watch?v=nyiNSFp2uf0",out_path);
+    std::ifstream f(out_path);
+    ASSERT_TRUE(f.good());
+}
+
+TEST(YoutubeDownload, DownloadAndResize)
+{
+    std::string out_path = {"${CMAKE_BINARY_DIR}/test/LouReed.mp4"};
+    YoutubeDownloader* d = new YoutubeDL("${CMAKE_SOURCE_DIR}/tools/YoutubeDL/youtube-dl");
+    d->download("https://www.youtube.com/watch?v=oG6fayQBm9w",out_path, 30, 90);
+    std::ifstream f(out_path);
+    ASSERT_TRUE(f.good());
 }
