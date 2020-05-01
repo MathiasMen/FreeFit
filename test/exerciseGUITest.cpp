c4a46a1b14255f8bc4a45162af687cada2e8673b
MathiasMen <mathiasmentler@web.de>
2020-05-01 11:21:36 +0200

    Added test for YoutubeDownloader and support for Qt in tests.

---
+++ test/exerciseGUITest.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include <QMainWindow>
+#include <QWidget>
+#include <QLabel>
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+    a.exec();
+}
