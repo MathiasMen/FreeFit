7c538108d0b258a59bdb51eca26fbca6bd2b8d94
MathiasMen <mathiasmentler@web.de>
2020-05-01 16:10:53 +0200

    Added Button in ExerciseEditor to create new Exercise.

--- include/exerciseeditor.h
+++ include/exerciseeditor.h
@@ -8,10 +8,12 @@
 #include <QLineEdit>
 #include <QListWidget>
 #include <QGridLayout>
-#include <QIcon>
 #include <QStyle>
 
 #include <iostream>
+#include <vector>
+
+#include "include/newexercisedemand.h"
 
 namespace FreeFit
 {
@@ -22,12 +24,13 @@ namespace FreeFit
         public:
             ExerciseEditor()
                 :   put_exercise_to_add_list(QApplication::style()->standardIcon(QStyle::SP_MediaSkipForward),"",this),
+                    create_new_exercise(QApplication::style()->standardIcon(QStyle::SP_FileDialogNewFolder),"",this),
                     name("Enter exercise name...",this),
                     video_url("Enter video url...",this),
                     base_volume("Enter repetitions/time...",this),
                     exercise_type_group(this),
-                    time_based_ex_button("Timebased Exercise"),
-                    rep_based_ex_button("Repetionbased Exercise"),
+                    time_based_ex_button("Timebased Exercise",this),
+                    rep_based_ex_button("Repetitionbased Exercise",this),
                     muscle_areas(this),
                     exercises_to_add(this)
             {
@@ -42,12 +45,14 @@ namespace FreeFit
                 ly->addWidget(&time_based_ex_button,++row_counter,1);
                 ly->addWidget(&rep_based_ex_button,row_counter,2);
                 ly->addWidget(&muscle_areas,0,0,row_counter+1,1);
-                ly->addWidget(&put_exercise_to_add_list,0,3,row_counter+1,1);
+                ly->addWidget(&put_exercise_to_add_list,1,3);
+                ly->addWidget(&create_new_exercise,2,3);
                 ly->addWidget(&exercises_to_add,0,4,row_counter+1,1);
                 this->setLayout(ly);
             }
         private:
             QPushButton put_exercise_to_add_list;
+            QPushButton create_new_exercise;
             QLineEdit name;
             QLineEdit video_url;
             QLineEdit base_volume;
@@ -56,6 +61,8 @@ namespace FreeFit
             QRadioButton rep_based_ex_button;
             QListWidget muscle_areas;
             QListWidget exercises_to_add;
+
+            std::vector<NewExerciseDemand> exercise_demands;
 
             QGridLayout* ly;
         };
