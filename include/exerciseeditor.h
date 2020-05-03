9e55b2500cb26c7dc03d34aefc9c58540fc3dc1b
MathiasMen <mathiasmentler@web.de>
2020-05-03 14:05:03 +0200

    Completely reworked ExerciseEditor.

--- include/exerciseeditor.h
+++ include/exerciseeditor.h
@@ -1,13 +1,18 @@
 #pragma once
 
 #include <QApplication>
+#include <QString>
+#include <QLabel>
 #include <QDialog>
 #include <QPushButton>
-#include <QRadioButton>
-#include <QButtonGroup>
 #include <QLineEdit>
-#include <QListWidget>
 #include <QGridLayout>
+#include <QVBoxLayout>
+#include <QStackedWidget>
+#include <QScrollArea>
+#include <QGroupBox>
+#include <QStyleOption>
+#include <QPainter>
 #include <QStyle>
 
 #include <iostream>
@@ -19,58 +24,165 @@ namespace FreeFit
 {
     namespace GUI
     {
-        class ExerciseEditor : public QDialog
+        class ClickableLabel : public QLabel
         {
+            Q_OBJECT
         public:
-            ExerciseEditor()
-                :   put_exercise_to_add_list(QApplication::style()->standardIcon(QStyle::SP_MediaSkipForward),"",this),
-                    create_new_exercise(QApplication::style()->standardIcon(QStyle::SP_FileDialogNewFolder),"",this),
-                    name("Enter exercise name...",this),
-                    video_url("Enter video url...",this),
-                    video_start_time("Enter video starting time...",this),
-                    video_end_time("Enter video end time...",this),
-                    base_volume("Enter repetitions/time...",this),
-                    exercise_type_group(this),
-                    time_based_ex_button("Timebased Exercise",this),
-                    rep_based_ex_button("Repetitionbased Exercise",this),
-                    muscle_areas(this),
-                    exercises_to_add(this)
-            {
-                exercise_type_group.addButton(&time_based_ex_button);
-                exercise_type_group.addButton(&rep_based_ex_button);
+            ClickableLabel(QString text,QWidget* parent):QLabel(text,parent){}
 
-                ly = new QGridLayout(this);
-                int row_counter = -1;
-                ly->addWidget(&name,++row_counter,1);
-                ly->addWidget(&video_url,++row_counter,1);
-                ly->addWidget(&video_start_time,++row_counter,1);
-                ly->addWidget(&video_end_time,++row_counter,1);
-                ly->addWidget(&base_volume,++row_counter,1);
-                ly->addWidget(&time_based_ex_button,++row_counter,1);
-                ly->addWidget(&rep_based_ex_button,row_counter,2);
-                ly->addWidget(&muscle_areas,0,0,row_counter,1);
-                ly->addWidget(&put_exercise_to_add_list,1,3);
-                ly->addWidget(&create_new_exercise,2,3);
-                ly->addWidget(&exercises_to_add,0,4,row_counter,1);
-                this->setLayout(ly);
+        protected:
+            void mousePressEvent(QMouseEvent* ev) override
+            {
+                emit labelClicked();
+                QLabel::mousePressEvent(ev);
+            }
+
+        signals:
+            void labelClicked();
+        };
+
+        class WriteableLine : public QLineEdit
+        {
+            Q_OBJECT
+        public:
+            WriteableLine(QString text, QWidget* parent):QLineEdit(parent){this->setText(text);}
+        protected:
+            void focusOutEvent(QFocusEvent* ev)
+            {
+                emit textMessageBecauseFocusLost(this->text());
+                QLineEdit::focusOutEvent(ev);
+            }
+        signals:
+            void textMessageBecauseFocusLost(QString t);
+        };
+
+        class EditableLine : public QStackedWidget
+        {
+            Q_OBJECT
+        public:
+            EditableLine(QString text,QWidget* parent):QStackedWidget(parent)
+            {
+                this->setFocusPolicy(Qt::StrongFocus);
+                l = new ClickableLabel(text,this);
+                le = new WriteableLine(text,this);
+                l->setStyleSheet("background-color:green;");
+                le->setStyleSheet("background-color:red;");
+                this->addWidget(l);
+                this->addWidget(le);
+                connect(l,&ClickableLabel::labelClicked,this,&EditableLine::showLineEdit);
+                connect(le,&WriteableLine::textMessageBecauseFocusLost,this,&EditableLine::showLabelAndSetText);
+            }
+        public slots:
+            void showLineEdit()
+            {
+                this->setCurrentWidget(le);
+            }
+
+            void showLabelAndSetText(QString t)
+            {
+                this->setCurrentWidget(l);
+                l->setText(t);
             }
         private:
-            QPushButton put_exercise_to_add_list;
-            QPushButton create_new_exercise;
-            QLineEdit name;
-            QLineEdit video_url;
-            QLineEdit video_start_time;
-            QLineEdit video_end_time;
-            QLineEdit base_volume;
-            QButtonGroup exercise_type_group;
-            QRadioButton time_based_ex_button;
-            QRadioButton rep_based_ex_button;
-            QListWidget muscle_areas;
-            QListWidget exercises_to_add;
+            ClickableLabel* l;
+            WriteableLine* le;
+        };
+        
+        class ExerciseItem : public QWidget
+        {
+            Q_OBJECT
+        public:
+            ExerciseItem(QWidget* parent):QWidget(parent)
+            {
+                this->setFocusPolicy(Qt::ClickFocus);
+                this->setStyleSheet("background-color:blue;");
 
-            std::vector<NewExerciseDemand> exercise_demands;
+                ly = new QGridLayout(this);
+                
+                name_label = new QLabel("Name:",this);
+                url_label = new QLabel("URL:",this);
+                start_time_label = new QLabel("Start Time:",this);
+                stop_time_label = new QLabel("Stop Time:",this);
+
+                name        = new EditableLine("...",this);
+                url         = new EditableLine("...",this);
+                start_time  = new EditableLine("...",this);
+                stop_time   = new EditableLine("...",this);
+
+                int row_counter = -1;
+                ly->addWidget(name_label,++row_counter,0);
+                ly->addWidget(url_label,++row_counter,0);
+                ly->addWidget(start_time_label,++row_counter,0);
+                ly->addWidget(stop_time_label,++row_counter,0);
+
+                row_counter = -1;
+                ly->addWidget(name,++row_counter,1);
+                ly->addWidget(url,++row_counter,1);
+                ly->addWidget(start_time,++row_counter,1);
+                ly->addWidget(stop_time,++row_counter,1);
+            }
+
+        private:
+
+            void paintEvent(QPaintEvent* e)
+            {
+                QStyleOption opt;
+                opt.init(this);
+                QPainter p(this);
+                style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
+            }
 
             QGridLayout* ly;
+
+            QLabel* name_label;
+            QLabel* url_label;
+            QLabel* start_time_label;
+            QLabel* stop_time_label;
+
+            EditableLine* name;
+            EditableLine* url;
+            EditableLine* start_time;
+            EditableLine* stop_time;
+        };
+
+        class ExerciseEditor : public QDialog
+        {
+            Q_OBJECT
+        public:
+            ExerciseEditor()
+            {                
+                ly = new QGridLayout(this);
+
+                add_button = new QPushButton("Add Exercise",this);
+                connect(add_button,&QPushButton::clicked,this,&ExerciseEditor::addExercise);
+
+                exercise_area = new QGroupBox("Exercises to download", this);
+                exercise_area_ly = new QVBoxLayout(exercise_area);
+                exercise_area->setLayout(exercise_area_ly);
+
+                ExerciseItem* e = new ExerciseItem(exercise_area);
+                exercise_area_ly->addWidget(e);
+
+                scroll_area = new QScrollArea(this);
+                scroll_area->setWidget(exercise_area);
+                scroll_area->setWidgetResizable(true);
+
+                ly->addWidget(add_button,0,0,1,1);
+                ly->addWidget(scroll_area,1,0,4,4);
+                this->setLayout(ly);
+            };
+        private:
+            QPushButton* add_button;
+            QScrollArea* scroll_area;
+            QGroupBox* exercise_area;
+            QVBoxLayout* exercise_area_ly;
+            QGridLayout* ly;
+        private slots:
+            void addExercise()
+            {
+                ExerciseItem* e = new ExerciseItem(exercise_area);
+                exercise_area_ly->addWidget(e);
+            }
         };
     }
 }
--- test/exerciseGUITest.cpp
+++ test/exerciseGUITest.cpp
@@ -12,6 +12,5 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
     QDialog* d = new FreeFit::GUI::ExerciseEditor();
-    d->resize(500,500);
     d->exec();
 }
