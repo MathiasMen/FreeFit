0c974eeac4b4fdc0c1008861bb1eb2c3a23486e6
MathiasMen <mathiasmentler@web.de>
2020-05-11 19:31:35 +0200

    Created test to verify correct appending to already existing Exercises-XML.

--- test/exerciseGUITest.cpp
+++ test/exerciseGUITest.cpp
@@ -205,3 +205,61 @@ TEST(NewExerciseDemandHandler, AddExecuteDemand)
     ASSERT_TRUE(f2.is_open());
     ASSERT_EQ(ss.str(),expected_exercises_xml);
 }
+
+TEST(NewExerciseDemandHandler, AddExercisesToExistingXML)
+{
+    FreeFit::Data::NewExerciseDemandHandler h("/Users/mathias/Documents/programming_workspace/FreeFit/build/test/AddExercisesToExistingXML.xml");
+    std::shared_ptr<FreeFit::GUI::NewExerciseDemand> d = std::make_shared<FreeFit::GUI::NewExerciseDemand>();
+    d->name = "Situp";
+    d->video_url = "https://www.youtube.com/watch?v=O-3uPB3fgys";
+    d->video_start_time = "50";
+    d->video_end_time = "53";
+    d->muscle_areas.push_back("Shoulder");
+    d->muscle_areas.push_back("Chest");
+    h.addDemand(d);
+    h.executeDemands();
+
+    h.addDemand(d);
+    h.executeDemands();
+
+    std::string expected_exercises_xml = 
+    "<EXERCISES>\n"
+    "  <EXERCISE>\n"
+    "    <NAME>\n"
+    "      Situp\n"
+    "    </NAME>\n"
+    "    <VIDEOPATH>\n"
+    "      /Users/mathias/Documents/programming_workspace/FreeFit/build/test/Situp_50_53.mp4\n"
+    "    </VIDEOPATH>\n"
+    "    <TRAINEDAREAS>\n"
+    "      <AREA>\n"
+    "        0\n"
+    "      </AREA>\n"
+    "      <AREA>\n"
+    "        4\n"
+    "      </AREA>\n"
+    "    </TRAINEDAREAS>\n"
+    "  </EXERCISE>\n"
+    "  <EXERCISE>\n"
+    "    <NAME>\n"
+    "      Situp\n"
+    "    </NAME>\n"
+    "    <VIDEOPATH>\n"
+    "      /Users/mathias/Documents/programming_workspace/FreeFit/build/test/Situp_50_53.mp4\n"
+    "    </VIDEOPATH>\n"
+    "    <TRAINEDAREAS>\n"
+    "      <AREA>\n"
+    "        0\n"
+    "      </AREA>\n"
+    "      <AREA>\n"
+    "        4\n"
+    "      </AREA>\n"
+    "    </TRAINEDAREAS>\n"
+    "  </EXERCISE>\n"
+    "</EXERCISES>\n";
+
+    std::ifstream f("/Users/mathias/Documents/programming_workspace/FreeFit/build/test/AddExercisesToExistingXML.xml");
+    std::stringstream ss;
+    ss << f.rdbuf();
+    ASSERT_EQ(ss.str(),expected_exercises_xml);
+}
