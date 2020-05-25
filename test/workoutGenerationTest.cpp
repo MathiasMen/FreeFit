88836ea0b627cbc7d343748d229e8feadcbca76e
MathiasMen <mathiasmentler@web.de>
2020-05-25 19:43:18 +0200

    Added test for generation of Workouts.

---
+++ test/workoutGenerationTest.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+
+#include <list>
+
+#include "include/workout.h"
+#include "include/xmlreader.h"
+
+int my_argc;
+char** my_argv;
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    my_argc = argc;
+    my_argv = argv;
+    return RUN_ALL_TESTS();
+}
+
+class WorkoutTestObject : public ::testing::Test
+{
+    protected:
+        FreeFit::Data::ExerciseXMLReader* r;
+        FreeFit::Data::AllExercisesWorkout* w;
+    virtual void SetUp()
+    {
+        r = new FreeFit::Data::ExerciseXMLReader("/Users/mathias/Documents/programming_workspace/FreeFit/test/WorkoutGenerationTest.xml");
+        w = new FreeFit::Data::AllExercisesWorkout(r->getExerciseList());
+    }
+};
+
+TEST_F(WorkoutTestObject,Launch)
+{
+    std::list<FreeFit::Data::Exercise> workout_list = w->generate();
+    ASSERT_EQ(workout_list.size(),6);
+}
