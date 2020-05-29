0b0d9c7734b8a8574e0064f289e95e50db427b6c
MathiasMen <mathiasmentler@web.de>
2020-05-29 15:33:11 +0200

    Removed unnecessary launch-command.

--- test/workoutWidgetTest.cpp
+++ test/workoutWidgetTest.cpp
@@ -34,6 +34,4 @@ TEST_F(WorkoutWidget,Launch)
 {
     QApplication a(my_argc,my_argv);
     FreeFit::GUI::WorkoutWidget* ww = new FreeFit::GUI::WorkoutWidget(w,nullptr);
-    ww->show();
-    a.exec();
 }
