8521f1fc09b1b9707f620ba5ff941bbbe72a58f4
MathiasMen <mathiasmentler@web.de>
2020-06-03 17:20:06 +0200

    Changed style of application.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QStyleFactory>
 
 #include <iostream>
 
@@ -8,8 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-
+    QApplication a(argc, argv);
+	QApplication::setStyle(QStyleFactory::create("Fusion"));
 	FreeFit::GUI::ProfileEditor* p = new FreeFit::GUI::ProfileEditor("/Users/mathias/Documents/programming_workspace/FreeFit/test/ProfileEditorReadInTest.xml");
 	p->exec();
 	FreeFit::GUI::ExerciseEditor* e = new FreeFit::GUI::ExerciseEditor(p->getCurrentlySelectedData());
