54c828eaff3e5789275fe02443bae3942f453070
MathiasMen <mathiasmentler@web.de>
2020-06-04 21:04:51 +0200

    Added title and validator to WorkoutGenerationWidget.

--- include/workoutgenerationwidget.h
+++ include/workoutgenerationwidget.h
@@ -6,6 +6,7 @@
 #include <QGridLayout>
 #include <QRadioButton>
 #include <QLineEdit>
+#include <QIntValidator>
 
 #include "include/exercise.h"
 #include "include/workout.h"
@@ -19,9 +20,12 @@ namespace FreeFit
         public:
             WorkoutGenerationWidget(QWidget* parent = nullptr) : QDialog(parent)
             {
+                this->setWindowTitle("Workout details");
                 ly = new QGridLayout(this);
 
                 number_of_lines = new QLineEdit("Enter number of rounds",this);
+                int_validator = new QIntValidator(1,50,this);
+                number_of_lines->setValidator(int_validator);
                 ly->addWidget(number_of_lines,0,0);
 
                 workout_types.push_back(std::make_shared<FreeFit::Data::AllExercisesWorkout>(std::list<FreeFit::Data::Exercise>()));
@@ -40,6 +44,7 @@ namespace FreeFit
         private:
             QGridLayout* ly;
             QRadioButton* all_exercises_workout;
+            QIntValidator* int_validator;
             QLineEdit* number_of_lines;
             std::list<std::shared_ptr<FreeFit::Data::WorkoutBase>> workout_types;
         };
