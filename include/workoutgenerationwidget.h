d40c67d29ab6fe0b12f0db4539186decf410ca26
MathiasMen <mathiasmentler@web.de>
2020-06-05 15:44:39 +0200

    Added mechanism to initialize and update data for WorkoutOption.

--- include/workoutgenerationwidget.h
+++ include/workoutgenerationwidget.h
@@ -19,7 +19,12 @@ namespace FreeFit
         class WorkoutOption : public QRadioButton
         {
         public:
-            WorkoutOption(QString text = "", QWidget* parent = nullptr) : QRadioButton(text,parent){}
+            WorkoutOption(QString text = "", std::shared_ptr<FreeFit::Data::WorkoutBase> w = nullptr, QWidget* parent = nullptr) : QRadioButton(text,parent), workout_data(w){}
+
+            void setPossibleExercises(std::list<FreeFit::Data::Exercise> e)
+            {
+                workout_data->setPossibleExercises(e);
+            }
         private:
             std::shared_ptr<FreeFit::Data::WorkoutBase> workout_data;
         };
@@ -39,9 +44,11 @@ namespace FreeFit
                 number_of_lines->setValidator(int_validator);
                 ly->addWidget(number_of_lines,0,0);
 
-                all_exercises_workout = new WorkoutOption("All Exercises",this);
+                std::shared_ptr<FreeFit::Data::AllExercisesWorkout> w1 = std::make_shared<FreeFit::Data::AllExercisesWorkout>(std::list<FreeFit::Data::Exercise>());
+                all_exercises_workout = new WorkoutOption("All Exercises",w1,this);
                 all_exercises_workout->setChecked(true);
                 ly->addWidget(all_exercises_workout,1,0);
+                workout_options.push_back(all_exercises_workout);
 
                 button_box = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
                 connect(button_box, &QDialogButtonBox::accepted, this, &WorkoutGenerationWidget::accept);
@@ -52,9 +59,10 @@ namespace FreeFit
 
             void setPossibleExercises(std::list<FreeFit::Data::Exercise> e)
             {
-                //for (auto w : workout_options)
-                    //w->setPossibleExercises(e);
+                for (auto w : workout_options)
+                    w->setPossibleExercises(e);
             }
+
         public slots:
 
             void accept() override
@@ -67,7 +75,7 @@ namespace FreeFit
             WorkoutOption* all_exercises_workout;
             QIntValidator* int_validator;
             QLineEdit* number_of_lines;
-            std::list<WorkoutOption> workout_options;
+            std::list<WorkoutOption*> workout_options;
             QDialogButtonBox* button_box;
         };
     }
