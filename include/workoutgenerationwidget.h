116a9b29dca50f5dda0c42c6bda5f83ee4997b71
MathiasMen <mathiasmentler@web.de>
2020-06-05 15:21:17 +0200

    Added custom class that holds the GUI-item of the exercise-option, as well as the actual workout-data object.

--- include/workoutgenerationwidget.h
+++ include/workoutgenerationwidget.h
@@ -16,6 +16,14 @@ namespace FreeFit
 {
     namespace GUI
     {
+        class WorkoutOption : public QRadioButton
+        {
+        public:
+            WorkoutOption(QString text = "", QWidget* parent = nullptr) : QRadioButton(text,parent){}
+        private:
+            std::shared_ptr<FreeFit::Data::WorkoutBase> workout_data;
+        };
+
         class WorkoutGenerationWidget : public QDialog
         {
         Q_OBJECT
@@ -31,8 +39,7 @@ namespace FreeFit
                 number_of_lines->setValidator(int_validator);
                 ly->addWidget(number_of_lines,0,0);
 
-                workout_types.push_back(std::make_shared<FreeFit::Data::AllExercisesWorkout>(std::list<FreeFit::Data::Exercise>()));
-                all_exercises_workout = new QRadioButton("All Exercises",this);
+                all_exercises_workout = new WorkoutOption("All Exercises",this);
                 all_exercises_workout->setChecked(true);
                 ly->addWidget(all_exercises_workout,1,0);
 
@@ -45,8 +52,8 @@ namespace FreeFit
 
             void setPossibleExercises(std::list<FreeFit::Data::Exercise> e)
             {
-                for (auto w : workout_types)
-                    w->setPossibleExercises(e);
+                //for (auto w : workout_options)
+                    //w->setPossibleExercises(e);
             }
         public slots:
 
@@ -57,10 +64,10 @@ namespace FreeFit
 
         private:
             QGridLayout* ly;
-            QRadioButton* all_exercises_workout;
+            WorkoutOption* all_exercises_workout;
             QIntValidator* int_validator;
             QLineEdit* number_of_lines;
-            std::list<std::shared_ptr<FreeFit::Data::WorkoutBase>> workout_types;
+            std::list<WorkoutOption> workout_options;
             QDialogButtonBox* button_box;
         };
     }
