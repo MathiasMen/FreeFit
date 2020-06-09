23d990cd5b6dac75c7bcd072623458b79fed3fdd
MathiasMen <mathiasmentler@web.de>
2020-06-09 19:22:52 +0200

    Changed the style of the app back to default in expectation of css-styles that have yet to be defined.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-	QApplication::setStyle(QStyleFactory::create("Fusion"));
 
 	MainWindow m("/Users/mathias/Documents/programming_workspace/FreeFit/test/ProfileEditorReadInTest.xml");
 	m.run();
