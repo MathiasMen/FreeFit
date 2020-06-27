e68e1fcff6e1f84fbe2cb6a0e44473cb4c027f3b
MathiasMen <mathiasmentler@web.de>
2020-06-27 17:42:06 +0200

    Further proof of concepts for materialbutton.

--- include/materialbutton.h
+++ include/materialbutton.h
@@ -1,17 +1,39 @@
 #pragma once
 
 #include <QWidget>
+#include <QString>
+#include <QLineEdit>
+#include <QPainter>
+#include <QRect>
+#include <QStyle>
+#include <QStyleOption>
+
+#include <iostream>
 
 namespace FreeFit
 {
     namespace GUI
     {
-        class MaterialButton : public QWidget
+        class MaterialButton : public QLineEdit
         {
+        Q_OBJECT
         public:
-            MaterialButton(QWidget* parent = nullptr) : QWidget(parent)
+            MaterialButton(QString t, QWidget* parent = nullptr) : QLineEdit(parent)
             {
-                
+                this->setStyleSheet("border: 2px solid lightblue");
+            }
+
+        protected:
+            void paintEvent(QPaintEvent* ev) override
+            {
+                QStyleOption opt;
+                opt.init(this);
+                QPainter p(this);
+                style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
+
+                QPainter painter(this);
+                painter.setPen(Qt::red);
+                painter.drawLine(0,0,this->rect().width(),this->rect().height());
             }
         };
     }
--- test/materialWidgetTest.cpp
+++ test/materialWidgetTest.cpp
@@ -22,8 +22,12 @@ TEST(MaterialWidget,Launch)
     QApplication a(my_argc,my_argv);
     QWidget* w = new QWidget;
     QVBoxLayout* ly = new QVBoxLayout(w);
-    FreeFit::GUI::MaterialButton* b = new FreeFit::GUI::MaterialButton(w);
-    ly->addWidget(b);
+    FreeFit::GUI::MaterialButton* b1 = new FreeFit::GUI::MaterialButton("Text1",w);
+    FreeFit::GUI::MaterialButton* b2 = new FreeFit::GUI::MaterialButton("Text2",w);
+    FreeFit::GUI::MaterialButton* b3 = new FreeFit::GUI::MaterialButton("Text3",w);
+    ly->addWidget(b1);
+    ly->addWidget(b2);
+    ly->addWidget(b3);
     w->show();
     a.exec();
 }
