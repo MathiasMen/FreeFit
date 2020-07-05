ec0a00e93e2f09aa3b262c22334bf8f7ccbdbca1
MathiasMen <mathiasmentler@web.de>
2020-07-05 15:34:33 +0200

    Added first drawing upon mouse-click to MaterialButton class.

--- include/materialbutton.h
+++ include/materialbutton.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include <QPushButton>
+#include <QMouseEvent>
+#include <QPainter>
+#include <QPen>
 
 #include <iostream>
+#include <functional>
 
 namespace FreeFit
 {
@@ -13,9 +17,38 @@ namespace FreeFit
             public:
                 MaterialButton(QString t, QWidget* parent = nullptr) : QPushButton(t,parent)
                 {
-                    this->setStyleSheet("background-color:white; color:black; border: 0px; padding: 0px;");
+                    this->setStyleSheet("background-color:white; color:black; border: 2px solid red; padding: 0px;");
                     this->setFlat(true);
                 }
+            protected:
+                void paintEvent(QPaintEvent* ev) override
+                {
+                    QPushButton::paintEvent(ev);
+                    QPainter painter(this);
+
+                    if(currentPaintFunction)
+                        currentPaintFunction(&painter);
+                }
+
+                void mousePressEvent(QMouseEvent* ev) override
+                {
+                    click_pos = ev->localPos();
+                    currentPaintFunction = std::bind(&MaterialButton::animateRipple,this,std::placeholders::_1);
+                    repaint();
+                    QPushButton::mousePressEvent(ev);
+                }
+
+            private:
+                void animateRipple(QPainter* painter)
+                {
+                    QPen pen = painter->pen();
+                    pen.setColor(Qt::red);
+                    painter->setPen(pen);
+                    painter->drawEllipse(click_pos,10,10);
+                }
+
+                QPointF click_pos;
+                std::function<void(QPainter*)> currentPaintFunction;
         };
     }
 }
