56c61f04d293bfcf72335b0b53038fc9243768b0
MathiasMen <mathiasmentler@web.de>
2020-07-05 16:09:39 +0200

    First ripple/halo effect animation in MaterialButton.

--- include/materialbutton.h
+++ include/materialbutton.h
@@ -4,6 +4,9 @@
 #include <QMouseEvent>
 #include <QPainter>
 #include <QPen>
+#include <QBrush>
+#include <QTimer>
+#include <QRadialGradient>
 
 #include <iostream>
 #include <functional>
@@ -14,11 +17,13 @@ namespace FreeFit
     {
         class MaterialButton : public QPushButton
         {
+            Q_OBJECT
             public:
-                MaterialButton(QString t, QWidget* parent = nullptr) : QPushButton(t,parent)
+                MaterialButton(QString t, QWidget* parent = nullptr) : QPushButton(t,parent),t_update(new QTimer)
                 {
-                    this->setStyleSheet("background-color:white; color:black; border: 2px solid red; padding: 0px;");
+                    this->setStyleSheet("background-color:white; color:black; border: 2px solid red; padding: 20px;");
                     this->setFlat(true);
+                    connect(this,SIGNAL(rippleFinished()),this,SLOT(handleRippleFinished()));
                 }
             protected:
                 void paintEvent(QPaintEvent* ev) override
@@ -32,23 +37,62 @@ namespace FreeFit
 
                 void mousePressEvent(QMouseEvent* ev) override
                 {
+                    rippleCounter = 0;
+                    rippleIntensityIncreasing = true;
                     click_pos = ev->localPos();
+                    connect(t_update,SIGNAL(timeout()),this,SLOT(updateRippleData()));
+                    connect(t_update,SIGNAL(timeout()),this,SLOT(repaint()));
+                    t_update->start(10);
                     currentPaintFunction = std::bind(&MaterialButton::animateRipple,this,std::placeholders::_1);
-                    repaint();
                     QPushButton::mousePressEvent(ev);
                 }
+            private slots:
+                void updateRippleData()
+                {
+                    const int rippleEnd = 10;
+                    if (rippleCounter < rippleEnd && rippleIntensityIncreasing)
+                        rippleCounter += 1;
+                    else if (rippleCounter <= rippleEnd && !rippleIntensityIncreasing)
+                        rippleCounter -= 1;
+                    else if (rippleCounter == rippleEnd)
+                        rippleIntensityIncreasing = false;
+                    else if (rippleCounter == 0 && !rippleIntensityIncreasing)
+                        emit rippleFinished();    
+                }
 
+                void handleRippleFinished()
+                {
+                    rippleCounter = 0;
+                    rippleIntensityIncreasing = true;
+                    click_pos = QPointF();
+                    disconnect(t_update,SIGNAL(timeout()),this,SLOT(updateRippleData()));
+                    t_update->stop();
+                    currentPaintFunction = 0;
+                }
             private:
                 void animateRipple(QPainter* painter)
                 {
                     QPen pen = painter->pen();
-                    pen.setColor(Qt::red);
+                    pen.setColor(Qt::white);
                     painter->setPen(pen);
-                    painter->drawEllipse(click_pos,10,10);
+/*                    
+                    QRadialGradient radialGrad(QPointF(click_pos.x()/2, click_pos.y()/2), 100);
+                    radialGrad.setColorAt(0,Qt::black);
+                    radialGrad.setColorAt(1,Qt::white);
+                    painter->setBrush(QBrush(radialGrad));
+*/
+                    painter->setBrush(QBrush(Qt::red));
+                    painter->setOpacity(rippleCounter*0.1);
+                    painter->drawEllipse(click_pos,50,50);
                 }
 
+                QTimer* t_update;
+                bool rippleIntensityIncreasing;
+                int rippleCounter;
                 QPointF click_pos;
                 std::function<void(QPainter*)> currentPaintFunction;
+            signals:
+                void rippleFinished();
         };
     }
 }
