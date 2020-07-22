edc4bc8953907279f6c5ce0d318c97a864e82ffe
MathiasMen <mathiasmentler@web.de>
2020-07-22 19:34:28 +0200

    Added second handler to MaterialSlider and set min and max values to which they can move.

--- include/materialslider.h
+++ include/materialslider.h
@@ -4,6 +4,8 @@
 #include <QMouseEvent>
 #include <QLabel>
 #include <QGridLayout>
+#include <QPainter>
+#include <QPen>
 
 #include <iostream>
 
@@ -19,6 +21,9 @@ namespace FreeFit
                 setFixedSize(10,20);
                 setStyleSheet("background-color:black;");
             }
+
+            void setMinX(int x){minVal = x;}
+            void setMaxX(int x){maxVal = x;}
         protected:
             void mousePressEvent(QMouseEvent* e)
             {
@@ -32,7 +37,8 @@ namespace FreeFit
                 {
                     QPoint old_pos = this->pos();
                     QPoint new_pos = mapToParent(e->pos() - offset);
-                    this->move(new_pos.x(),old_pos.y());
+                    if (new_pos.x() < maxVal && new_pos.x() > minVal)
+                        this->move(new_pos.x(),old_pos.y());
                 }
             }
 
@@ -42,6 +48,8 @@ namespace FreeFit
                 offset = QPoint();
             }
         private:
+            int minVal;
+            int maxVal;
             QPoint offset;
             bool pressed = false;
         };
@@ -54,12 +62,34 @@ namespace FreeFit
                 ly = new QGridLayout(this);
                 setFixedSize(200,50);
                 setStyleSheet("background-color:red;");
+                
                 left_handle = new MaterialSliderHandle("",this);
+                right_handle = new MaterialSliderHandle("",this);
+                
+                left_handle->setMinX(distance_line_to_border);
+                left_handle->setMaxX(this->rect().width()-distance_line_to_border);
+                right_handle->setMinX(distance_line_to_border);
+                right_handle->setMaxX(this->rect().width()-distance_line_to_border);
+
                 left_handle->setStyleSheet("background-color:black;");
-                ly->addWidget(left_handle);
+                right_handle->setStyleSheet("background-color:black;");
+                ly->addWidget(left_handle,0,0);
+                ly->addWidget(right_handle,0,0);
+            }
+        protected:
+            void paintEvent(QPaintEvent* e) override
+            {
+                QWidget::paintEvent(e);
+                QPen pen;
+                pen.setColor(QColor(Qt::black));
+                QPainter painter(this);
+                painter.setPen(pen);
+                painter.drawLine(distance_line_to_border,this->rect().height()/2,this->rect().width()-distance_line_to_border,this->rect().height()/2);
             }
         private:
+            const int distance_line_to_border = 20;
             MaterialSliderHandle* left_handle;
+            MaterialSliderHandle* right_handle;
             QGridLayout* ly;
         };
     }
