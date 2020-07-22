5e5dfe44177ef0c4a3121d9eaaf98471a5517289
MathiasMen <mathiasmentler@web.de>
2020-07-22 17:11:46 +0200

    Added MaterialSlider class and respective test.

---
+++ include/materialslider.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <QWidget>
+
+namespace FreeFit
+{
+    namespace GUI
+    {
+        class MaterialSlider : public QWidget
+        {
+        public:
+            MaterialSlider(QWidget* parent = nullptr):QWidget(parent){}
+        };
+    }
+}
---
+++ test/materialSliderTest.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+
+#include <QApplication>
+
+#include "include/materialslider.h"
+
+int my_argc;
+char** my_argv;
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    my_argc = argc;
+    my_argv = argv;
+    return RUN_ALL_TESTS();
+}
+
+class materialSliderTest : public ::testing::Test
+{
+    protected:
+    virtual void SetUp()
+    {
+    }
+};
+
+TEST_F(materialSliderTest,Init)
+{
+    QApplication a(my_argc,my_argv);
+    FreeFit::GUI::MaterialSlider* s = new FreeFit::GUI::MaterialSlider;
+    s->show();
+    a.exec();
+}
