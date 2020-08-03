e69c3db1a783e4ea8d74d509b282ab3ff5752072
MathiasMen <mathiasmentler@web.de>
2020-08-03 15:01:34 +0200

    Fixed bug that prevented deselection of other profiles when one was clicked.

--- src/profileeditor.cpp
+++ src/profileeditor.cpp
@@ -20,6 +20,7 @@ namespace FreeFit
                 MaterialClip* m = new MaterialClip(profile_name);
                 content_ly->addWidget(m);
                 profiles.push_back(m);
+                connect(m,SIGNAL(clicked(MaterialClip*)),this,SLOT(deselectOthers(MaterialClip*)));
                 connect(m,SIGNAL(clicked(MaterialClip*)),this,SLOT(select(MaterialClip*)));
             }
 
