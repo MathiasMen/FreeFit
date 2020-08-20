6bff56847adc8f3656db26af2a3a0e9087f1eac9
MathiasMen <mathiasmentler@web.de>
2020-08-20 18:30:05 +0200

    Replaced .jpg with .webp since this is supported by youtube-dl.

--- src/downloadexercisedemandhandler.in.cpp
+++ src/downloadexercisedemandhandler.in.cpp
@@ -30,7 +30,7 @@ namespace FreeFit
                 e.addTrainedMuscle(stringToMuscleGroup(a));
             std::string out_path_base = "${CMAKE_SOURCE_DIR}/build/test/" + d->name + "_" + d->video_start_time + "_" + d->video_end_time;
             std::string out_path_video = out_path_base + ".mp4";
-            std::string out_path_thumbnail = out_path_base + ".jpg";
+            std::string out_path_thumbnail = out_path_base + ".webp";
             downloadVideo(d->video_url,out_path_video);
             out_path_video = cutVideo(out_path_video,d->video_start_time,d->video_end_time);
             e.setVideoURL(d->video_url);
