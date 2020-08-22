821f0602daef4dd9b76da6dbfa586092532fc434
MathiasMen <mathiasmentler@web.de>
2020-08-22 18:22:29 +0200

    Further refactoring of functionality into Hashtagbar-class.

--- include/hashtagbar.h
+++ include/hashtagbar.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <QWidget>
+#include <QHBoxLayout>
+
+#include "include/toggleablelabel.h"
 
 namespace FreeFit
 {
@@ -9,7 +12,37 @@ namespace FreeFit
         class HashtagBar : public QWidget
         {
             public:
-                HashtagBar(QWidget parent = nullptr) : QWidget(parent){}
+                HashtagBar(QWidget* parent = nullptr) : QWidget(parent)
+                {
+                    ly = new QHBoxLayout(this);
+                }
+
+                void addHashtag(std::string tag_str)
+                {
+                    ToggleableLabel* t = new ToggleableLabel("#" + QString::fromStdString(tag_str),this);
+                    ly->addWidget(t,0,Qt::AlignLeft);
+                    hashtag_labels.push_back(t);
+                }
+
+                std::list<std::string> getToggledStrings()
+                {
+                    std::list<std::string> l;
+                    for (auto label : hashtag_labels)
+                        if(label->isToggled())
+                            l.push_back(label->text().remove(QChar('#')).toStdString());
+                    return l;
+                }
+
+                void toggleHashtag(std::string tag_str)
+                {
+                    for (auto h : hashtag_labels)
+                        if (h->text().remove(QChar('#')).toStdString() == tag_str)
+                            h->click();
+                }
+
+            private:
+                QHBoxLayout* ly;
+                std::vector<ToggleableLabel*> hashtag_labels;
         };
     }
 }
