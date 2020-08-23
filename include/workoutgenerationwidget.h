81bd3914ecaf80d66877083bacc2717b5364f947
MathiasMen <mathiasmentler@web.de>
2020-08-23 15:21:59 +0200

    Moved number_of_rounds to WorkoutOptionsBase, removed it from WorkoutGenerationWidget. Updated functions that set the number of rounds to now operate on the currently selected workout-option.

--- include/workoutgenerationwidget.h
+++ include/workoutgenerationwidget.h
@@ -31,11 +31,22 @@ namespace FreeFit
 
         class WorkoutOptionBase : public QRadioButton
         {
-            friend WorkoutGenerationWidgetValidator;
+        Q_OBJECT
+        friend WorkoutGenerationWidgetValidator;
         public:
             WorkoutOptionBase(QString text = "", std::shared_ptr<FreeFit::Data::WorkoutBase> w = nullptr, QWidget* parent = nullptr) : QRadioButton(text,parent), workout_data(w)
             {
+                possible_options_widget = new QWidget;
+                QVBoxLayout* ly = new QVBoxLayout(possible_options_widget);
 
+                number_of_rounds = new MaterialTextField("Number of rounds",this);
+                std::regex rounds_regex("[1-9]");
+                auto func_rounds_regex = [rounds_regex](std::string s)->bool{return std::regex_match(s,rounds_regex);};
+                number_of_rounds->setValidationFunction(func_rounds_regex);
+
+                ly->addWidget(number_of_rounds);
+
+                connect(number_of_rounds,&QLineEdit::textChanged,this,&WorkoutOptionBase::numberOfRoundsChanged);
             }
 
             void setPossibleExercises(std::list<FreeFit::Data::Exercise> e)
@@ -45,7 +56,10 @@ namespace FreeFit
         
             std::list<FreeFit::Data::Exercise> getExercisesPerRound(){return workout_data->getExercisesPerRound();}
 
-            void setRounds(unsigned int r){workout_data->setRounds(r);}
+            void setRounds(unsigned int r)
+            {
+                number_of_rounds->setText(QString::fromStdString(std::to_string(r)));
+            }
 
             unsigned int getRounds(){return workout_data->getRounds();}
 
@@ -60,39 +74,26 @@ namespace FreeFit
                 else
                     return nullptr;
             }
+        private slots:
+            void numberOfRoundsChanged()
+            {
+                if (!number_of_rounds->text().isEmpty() && number_of_rounds->validateText())
+                    workout_data->setRounds(std::stoi(number_of_rounds->text().toStdString()));
+            }
         protected:
             std::shared_ptr<FreeFit::Data::WorkoutBase> workout_data;
             QWidget* possible_options_widget = nullptr;
+            MaterialTextField* number_of_rounds;
         };
 
         class AllExercisesWorkoutOption : public WorkoutOptionBase
         {
         Q_OBJECT
+        friend WorkoutGenerationWidgetValidator;
         public:
             AllExercisesWorkoutOption(QString text = "", std::shared_ptr<FreeFit::Data::WorkoutBase> w = nullptr, QWidget* parent = nullptr) : WorkoutOptionBase(text,w,parent)
             {
-                possible_options_widget = new QWidget;
-                QVBoxLayout* ly = new QVBoxLayout(possible_options_widget);
-
-                number_of_rounds = new MaterialTextField("Number of rounds",this);
-                std::regex rounds_regex("[1-9]");
-                auto func_rounds_regex = [rounds_regex](std::string s)->bool{return std::regex_match(s,rounds_regex);};
-                number_of_rounds->setValidationFunction(func_rounds_regex);
-
-                ly->addWidget(number_of_rounds);
-
-                connect(number_of_rounds,&QLineEdit::textChanged,this,&AllExercisesWorkoutOption::numberOfRoundsChangedSlot);
             }
-        signals:
-            void numberOfRoundsChanged(int);
-        private slots:
-            void numberOfRoundsChangedSlot()
-            {
-                if (!number_of_rounds->text().isEmpty() && number_of_rounds->validateText())
-                    emit numberOfRoundsChanged(std::stoi(number_of_rounds->text().toStdString()));
-            }
-        private:
-            MaterialTextField* number_of_rounds;
         };
 
         class WorkoutGenerationWidget : public MaterialDialog
@@ -104,25 +105,19 @@ namespace FreeFit
             {
                 ly = new QGridLayout(this);
 
-                number_of_rounds = new MaterialTextField("Number of rounds",this);
-                int_validator = new QIntValidator(1,50,this);
-                number_of_rounds->setValidator(int_validator);
-                ly->addWidget(number_of_rounds,0,0);
-
                 std::shared_ptr<FreeFit::Data::AllExercisesWorkout> w1 = std::make_shared<FreeFit::Data::AllExercisesWorkout>(std::list<FreeFit::Data::Exercise>());
                 all_exercises_workout = new AllExercisesWorkoutOption("All Exercises",w1,this);
-                ly->addWidget(all_exercises_workout,1,0);
+                ly->addWidget(all_exercises_workout,0,0);
                 workout_options.push_back(all_exercises_workout);
 
                 std::shared_ptr<FreeFit::Data::FilteredByMusclesWorkout> w2 = std::make_shared<FreeFit::Data::FilteredByMusclesWorkout>(std::list<FreeFit::Data::Exercise>());
                 filtered_exercises_workout = new WorkoutOptionBase("Filtered by muscle groups",w2,this);
-                ly->addWidget(filtered_exercises_workout,2,0);
+                ly->addWidget(filtered_exercises_workout,1,0);
                 workout_options.push_back(filtered_exercises_workout);
 
                 all_exercises_workout->setChecked(true);
 
-                connect(number_of_rounds,&QLineEdit::textChanged,this,&WorkoutGenerationWidget::numberOfRoundsChanged);
-                number_of_rounds->setText(QString::fromStdString(std::to_string(3)));
+                setNumberOfRounds(3);
 
                 next_page_button = new ControlButton("Workout",ControlButton::ForwardButton,ControlButton::Primary,this);
                 connect(next_page_button, &QPushButton::clicked, this, &QDialog::accept);
@@ -157,13 +152,13 @@ namespace FreeFit
                         return w;
                 return nullptr;
             }
-        public slots:
-            void numberOfRoundsChanged()
+
+            void setNumberOfRounds(int n)
             {
-                if (!number_of_rounds->text().isEmpty())
-                    getSelectedWorkout()->setRounds(std::stoi(number_of_rounds->text().toStdString()));
+                getSelectedWorkout()->setRounds(n);
             }
 
+        public slots:
             void accept() override
             {
                 getSelectedWorkout()->generateWorkout();
@@ -174,8 +169,6 @@ namespace FreeFit
             QGridLayout* ly;
             WorkoutOptionBase* all_exercises_workout;
             WorkoutOptionBase* filtered_exercises_workout;
-            QIntValidator* int_validator;
-            MaterialTextField* number_of_rounds;
             std::list<WorkoutOptionBase*> workout_options;
             ControlButton* next_page_button;
             ControlButton* previous_page_button;
@@ -186,7 +179,7 @@ namespace FreeFit
         public:
             WorkoutGenerationWidgetValidator(WorkoutGenerationWidget* t_w) : QObject(), w(t_w){}
 
-            void setNumberOfRounds(unsigned int r){w->number_of_rounds->setText(QString::fromStdString(std::to_string(r)));}
+            void setNumberOfRounds(unsigned int r){w->all_exercises_workout->number_of_rounds->setText(QString::fromStdString(std::to_string(r)));}
         private:
             WorkoutGenerationWidget* w;
         };
