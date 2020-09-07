8e3780587093667fc24abfa2f2a80e109bae06b9
MathiasMen <mathiasmentler@web.de>
2020-09-07 17:29:44 +0200

    Added test for TDD of ProfileItem class and amended constructor.

--- include/profileitem.h
+++ include/profileitem.h
@@ -8,7 +8,13 @@ namespace FreeFit
     {
         class ProfileItem : public QWidget
         {
+        public:
+            ProfileItem(QString t_name = "new", QWidget* t_parent = nullptr) : QWidget(t_parent),name(t_name.toStdString())
+            {
 
+            }
+        private:
+            std::string name;
         };
     }
 }
---
+++ test/profileItemTest.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+
+#include <QApplication>
+#include <QWidget>
+#include <QVBoxLayout>
+
+#include "include/profileitem.h"
+
+int my_argc;
+char** my_argv;
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    my_argc = argc;
+    my_argv = argv;
+    return RUN_ALL_TESTS();
+}
+
+TEST(MaterialWidget,Launch)
+{
+    QApplication a(my_argc,my_argv);
+
+    QWidget* w = new QWidget;
+    QVBoxLayout* ly = new QVBoxLayout(w);
+    w->setStyleSheet("background-color:white");
+    FreeFit::GUI::ProfileItem b1("Text1",w);
+    FreeFit::GUI::ProfileItem b2("Text2",w);
+    FreeFit::GUI::ProfileItem b3("Text3",w);
+    ly->addWidget(&b1);
+    ly->addWidget(&b2);
+    ly->addWidget(&b3);
+    w->show();
+    a.exec();
+}
