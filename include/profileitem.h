5bd6b4bd92d6ff815a622dc3bb30117013252476
MathiasMen <mathiasmentler@web.de>
2020-09-07 19:50:56 +0200

    Added text to ProfileItem.

--- include/profileitem.h
+++ include/profileitem.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <QWidget>
+#include <QGridLayout>
+#include <QLabel>
 
 namespace FreeFit
 {
@@ -9,12 +11,23 @@ namespace FreeFit
         class ProfileItem : public QWidget
         {
         public:
-            ProfileItem(QString t_name = "new", QWidget* t_parent = nullptr) : QWidget(t_parent),name(t_name.toStdString())
+            ProfileItem(QString t_name = "new", QWidget* t_parent = nullptr) : QWidget(t_parent)
             {
-
+                const int width = 50;
+                const int height = 50;
+                this->setFixedSize(width,height);
+                ly = new QGridLayout(this);
+                name_label = new QLabel(this);
+                name_label->setFixedSize(width,height);
+                setName(t_name);
+            }
+            void setName(QString name)
+            {
+                name_label->setText(name);
             }
         private:
-            std::string name;
+            QGridLayout* ly;
+            QLabel* name_label;
         };
     }
 }
