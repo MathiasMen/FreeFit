8c3eb0979f564f9d1c807f662c474cf45cb694dd
MathiasMen <mathiasmentler@web.de>
2020-09-13 18:18:25 +0200

    Removed almost duplicate test from ProfileItemTest.

--- test/profileItemTest.cpp
+++ test/profileItemTest.cpp
@@ -17,22 +17,6 @@ int main(int argc, char** argv)
     return RUN_ALL_TESTS();
 }
 
-TEST(ProfileItem,Launch)
-{
-    QApplication a(my_argc,my_argv);
-
-    QWidget* w = new QWidget;
-    QVBoxLayout* ly = new QVBoxLayout(w);
-    FreeFit::GUI::ProfileItem b1("Text1",w);
-    FreeFit::GUI::ProfileItem b2("Text2",w);
-    FreeFit::GUI::ProfileItem b3("Some really long text.",w);
-    ly->addWidget(&b1);
-    ly->addWidget(&b2);
-    ly->addWidget(&b3);
-    w->show();
-    a.exec();
-}
-
 TEST(ProfileItemGroup,Launch)
 {
     QApplication a(my_argc,my_argv);
