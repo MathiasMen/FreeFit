0197badf1b0dc16f9d51eb2241b902f101588e37
MathiasMen <mathiasmentler@web.de>
2020-10-03 09:07:56 +0200

    Added functionality to Profile class that can create and check for a particular type of Profile.

--- include/profile.h
+++ include/profile.h
@@ -8,8 +8,36 @@ namespace FreeFit
     {
         class Profile
         {
+            static Profile newProfileProfile()
+            {
+                return Profile("New Profile","#ff0000","","","",1.0);
+            }
+
             public:
                 Profile(){};
+                Profile(std::string n,
+                        std::string c,
+                        std::string p_path,
+                        std::string date,
+                        std::string db_path,
+                        double perf)
+                        :name(n),
+                        color(c),
+                        picture_path(p_path),
+                        date_of_last_workout(date),
+                        path_to_exercise_database(db_path),
+                        performance_factor(perf){}
+
+                bool isNewProfileProfile()
+                {
+                    Profile p = newProfileProfile();
+                    return (name == p.getName() &&
+                            color == p.getColor() &&
+                            picture_path == p.getPicturePath() &&
+                            date_of_last_workout == p.getDateLastWorkout() &&
+                            path_to_exercise_database == p.getPathToExerciseDB() &&
+                            performance_factor == p.getPerformanceFactor());
+                }
 
                 void setName(std::string n){name = n;}
                 std::string getName(){return name;}
